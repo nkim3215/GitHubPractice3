5c371905407efa7d116f9d2ba4088aa26e3b7e3e
JoelJohn <jjohnn3593@stu.d214.org>
2022-11-17 11:22:34 -0600

    part 1 done with protocol stubs

--- GitHubPractice3/ViewController.swift
+++ GitHubPractice3/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+ 
+    
+  
+    
+    
+    @IBOutlet weak  var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
+    
+    var array = String([])
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource
     }
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
 }
 
